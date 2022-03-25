bae1542f7a738f0aff09d6a36ec00cb3eb25d6d2
Robin Muller <robinmuller75009@icloud.com>
2022-03-25 20:00:01 +0100

    add database configuration

--- web_application_reporting/settings.py
+++ web_application_reporting/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'web_application_reporting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'war',
+        'USER': 'robinmuller',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
